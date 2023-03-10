aeb26e419507e31743ee97de4ae5884cd7d83c51
Robin Risto Kivit <nrobinkivit@gmail.com>
2023-03-10 13:09:25 +0200

    Made the new website.

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,83 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import react, { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
+  const [buttonSize, setButtonSize] = useState({ width: 100, height: 50 });
+  const [bgColor, setbgColor] = useState(0);
+  const [buttonTextIndex, setButtonTextIndex] = useState(1);
+  const ClickMeList = [
+    'Click me', 
+    'Vajuta mind', 
+    '点击我', 
+    'Klikněte mě',
+    'Klik mig',
+    'Klik me',
+    'Klikkaa minua',
+    'Cliquez-moi',
+    'Klick mich',
+    'Kattints rám'
+  ];
+  const [buttonText, setButtonText] = useState(ClickMeList[0]);
+  const [shouldShake, setShouldShake] = useState(false);
+  
+
+  // useEffect(() => { 
+  //   //setButtonSize({ width: window.innerWidth / 10, height: window.innerHeight / 20 });
+  //   function handleResize() { 
+  //   setButtonSize({ width: window.innerWidth / 10, height: window.innerHeight / 20 });
+  //   }
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = `rgba(0,0,0,${bgColor})`;
+  }, [bgColor]);
+
+  useEffect(() => {
+    if (shouldShake) {
+      const timeoutId = setTimeout(() => {
+        setShouldShake(false);
+      }, 150);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [shouldShake]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setbgColor((bgColor) => Math.max(bgColor - 0.01, 0));
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleHover = () => {
+    setButtonSize({ width: buttonSize.width * 1.2, height: buttonSize.height * 1.2 });
+  };
+
+  const handleLeave = () => {
+    setButtonSize({ width: buttonSize.width / 1.2, height: buttonSize.height / 1.2 });
+  };
+
+  const handleDown = () => {
+    setButtonSize({ width: buttonSize.width / 1.1 , height: buttonSize.height / 1.1 });
+  };
+
+  const handleUp = () => {
+    setButtonSize({ width: buttonSize.width * 1.1 , height: buttonSize.height * 1.1 });
+  };
+
+  const handleButtonClick = () => {
+    setbgColor(bgColor > 1 ? bgColor : bgColor + 0.1);
+    setButtonTextIndex((buttonTextIndex + 1) % ClickMeList.length);
+    setButtonText(ClickMeList[buttonTextIndex]);
+    setShouldShake(true);
+  };
+
   return (
     <>
       <Head>
@@ -15,107 +88,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+        <div>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+            <button
+              className={shouldShake ? 'shake' : ''}
+              style={{ width: buttonSize.width, 
+                height: buttonSize.height, 
+                transition: 'all 0.2s ease-in-out'
+              }}
+              onMouseEnter={handleHover}
+              onMouseLeave={handleLeave}
+              onMouseDown={handleDown}
+              onMouseUp={handleUp}
+              onClick={handleButtonClick}
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+              <div>{buttonText}</div>
+              <div>{Math.round(bgColor*100)/100}</div>
+            </button>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
